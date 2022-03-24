658aa2c566e940a0ed5ec6e2603477b97b68e1c6
LEO\leog4 <leog4za@gmail.com>
2022-03-23 21:45:49 -0500

    front-end initialization

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,21 @@
-import './App.css';
+import { Home } from './Pages';
+import ScrollToTop from './Components/ScrollToTop';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ScrollToTop>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </ScrollToTop>
+    </Router>
   );
-}
+};
 
 export default App;
