9830c8a2aab0ff1917c463d73a32e30e1be24b27
LEO\leog4 <leog4za@gmail.com>
2022-03-24 23:12:33 -0500

    services header

--- client/src/Pages/Services.jsx
+++ client/src/Pages/Services.jsx
@@ -3,7 +3,7 @@ import Navbar from '../Components/NavBar';
 
 const LinearGradient =
   'linear-gradient(180deg, #303030 0%, rgba(190, 120, 110, 0.12) 20%)';
-  
+
 export default function Services() {
   return (
     <>
@@ -11,17 +11,17 @@ export default function Services() {
       <div
         className="min-h-full h-[40rem] w-full bg-cover bg-center"
         style={{
-          backgroundImage: `${LinearGradient},url('https://images.unsplash.com/photo-1439539698758-ba2680ecadb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')`,
+          backgroundImage: `${LinearGradient},url('https://images.unsplash.com/photo-1496559249665-c7e2874707ea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')`,
         }}
       >
         <div className="max-w-6xl h-full mx-auto">
           <div className="flex w-3/4 sm:w-1/2 mx-auto flex-col justify-center items-center h-full text-white">
             <h2 className="text-xl uppercase font-semibold tracking-wider mb-5 mt-36">
-              - Gallery -{' '}
+              - Services -{' '}
             </h2>
-            <h1 className="text-6xl font-semibold tracking-wide mb-5">
+            {/* <h1 className="text-6xl font-semibold tracking-wide mb-5">
               Collection
-            </h1>
+            </h1> */}
             <p className="text-sm px-0 sm:px-24 text-center">
               When, while the lovely valley teems with vapour around me, and the
               meridian sun strikes the upper surface of the impenetrable foliage
