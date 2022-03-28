878ea2b37995006ab999df8eb44c47132a8bda16
LEO\leog4 <leog4za@gmail.com>
2022-03-27 20:59:12 -0500

    heroku setup

--- client/src/Pages/Booking.jsx
+++ client/src/Pages/Booking.jsx
@@ -89,7 +89,7 @@ export default function Booking() {
                     Location
                   </label>
                   <Autocomplete
-                    apiKey={process.env.GMAP_API_KEY}
+                    apiKey={process.env.GMAP_API_KEY.toString()}
                     className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                     onPlaceSelected={(place) => {
                       console.log(place);
