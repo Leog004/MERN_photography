8735ab3128951fc0c4c22c7c3904c36f062d8ab7
LEO\leog4 <leog4za@gmail.com>
2022-03-27 21:09:58 -0500

    heroku setup

--- client/src/Pages/Booking.jsx
+++ client/src/Pages/Booking.jsx
@@ -16,6 +16,8 @@ const datesToAddClassTo = [
   '2022-04-12T00:00:00',
 ];
 
+const Key = process.env.GMAP_API_KEY;
+
 function isSameDay(a, b) {
   const formattedDate = format(parseISO(a), 'MM/dd/yyyy');
   const formattedDateCalender = format(
@@ -89,7 +91,7 @@ export default function Booking() {
                     Location
                   </label>
                   <Autocomplete
-                    apiKey={process.env.GMAP_API_KEY.toString()}
+                    apiKey={Key}
                     className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                     onPlaceSelected={(place) => {
                       console.log(place);
