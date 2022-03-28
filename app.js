9b971f52b06e419d982d454891362852e76f1d64
LEO\leog4 <leog4za@gmail.com>
2022-03-27 22:32:54 -0500

    api

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.use('/api/v1/users', userRouter);
 app.use('/api/v1/bookings', bookingRouter);
 
 app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 app.use(globalErrorHandler);
