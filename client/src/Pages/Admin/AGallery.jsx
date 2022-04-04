ec16fa7dfe5d25fa37accf1716ecfb4495b5ba0a
Leo Garza <lgarza@equibrand.com>
2022-04-04 15:47:32 -0500

    add new gallery

--- client/src/Pages/Admin/AGallery.jsx
+++ client/src/Pages/Admin/AGallery.jsx
@@ -4,6 +4,8 @@ import axios from 'axios';
 
 export default function AGallery() {
   const [gallery, setGallery] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [newGalleryName, setNewGalleryName] = useState('');
 
   useEffect(() => {
     const getGallery = async () => {
@@ -19,55 +21,119 @@ export default function AGallery() {
     getGallery();
   }, []);
 
+  const handleOnClick = (e) => {
+    e.preventDefault();
+    setShowForm((el) => !el);
+  };
+
   return (
     <div className="relative">
       <Navbar />
-
-      <div class="relative overflow-x-auto shadow-md sm:rounded-lg my-20">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" class="px-6 py-3">
-                Gallery
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Images
-              </th>
-              <th scope="col" class="px-6 py-3">
-                <span class="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {gallery.map((el) => (
-              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                <td class="px-6 py-4">{el.title}</td>
-                <td class="px-6 py-4 text-right">
-                  <section
-                    id="gallery"
-                    className="max-w-6xl mx-auto h-full py-5"
-                  >
-                    <div className="grid grid-1 sm:grid-cols-12 gap-x-8 gap-y-10">
-
-                    {
-                        el.images.map((img) => (
-                            <div className="flex w-full h-auto px-4 sm:px-0">
-                        <img
-                          alt={'asd'}
-                          src={img}
-                        />
-                      </div>
-                        ))
-                    }
-                     
-                    </div>
-                  </section>
-                </td>
+      {!showForm ? (
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg my-20">
+          <div className="container mx-auto shadow-lg py-2 w-full">
+            <div className="flex items-center h-full justify-end">
+              <button
+                onClick={handleOnClick}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              >
+                Add Gallery
+              </button>
+            </div>
+          </div>
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Gallery
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Images
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  <span className="sr-only">Edit</span>
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+            </thead>
+            <tbody>
+              {gallery.map((el) => (
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                  <td className="px-6 py-4">{el.title}</td>
+                  <td className="px-6 py-4 text-right">
+                    <section
+                      id="gallery"
+                      className="max-w-6xl mx-auto h-full py-5"
+                    >
+                      <div className="grid grid-1 sm:grid-cols-12 gap-x-8 gap-y-10">
+                        {el.images.map((img) => (
+                          <div className="flex w-full h-auto px-4 sm:px-0">
+                            <img alt={'asd'} src={img} />
+                          </div>
+                        ))}
+                      </div>
+                    </section>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg my-20">
+        
+        <div className="flex flex-col w-full items-center justify-center">
+        <button
+                onClick={handleOnClick}
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-10"
+              >
+               Cancel
+              </button>
+          <div class="mb-10 w-96">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="username"
+            >
+              Gallery Name
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              name="gallery"
+              id="gallery"
+              type="text"
+              placeholder="gallery name"
+            />
+          </div>
+          <div className="mb-3 w-96">
+            <label
+              for="formFileMultiple"
+              className="form-label inline-block mb-2 text-gray-700"
+            >
+              Multiple files input example
+            </label>
+            <input
+              className="form-control
+              block
+              w-full
+              px-3
+              py-1.5
+              text-base
+              font-normal
+              text-gray-700
+              bg-white bg-clip-padding
+              border border-solid border-gray-300
+              rounded
+              transition
+              ease-in-out
+              m-0
+            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              type="file"
+              id="formFileMultiple"
+              multiple
+            />
+          </div>
+        </div>
+        </div>
+      )}
     </div>
   );
 }
